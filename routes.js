9515d0a0444377cda80968542435c593fdae905a
jschutts <jschutts@live.unc.edu>
2016-07-27 09:27:23 -0400

    progress

--- routes.js
+++ routes.js
@@ -134,7 +134,7 @@ module.exports = function(app,io, request, app2, apiai){
 			if (data.msg.lastIndexOf("ADD") == -1)
 	        	socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
 	        if (data.user != 'bot'){
-	        	console.log(data.msg);
+	        	console.log(data.user);
 	            var request2 = app2.textRequest(data.msg,
 	            	{
 	            		sessionId: socket.id
